063b30dc7dfd5e2465277fcf886df919515c0553
DingSJ <DingSJ@github.com>
2021-02-03 21:53:02 +0800

    更新设计模式

---
+++ src/main/java/org/test/tmp/IntegerTest.java
@@ -0,0 +1,12 @@
+package org.test.tmp;
+
+import org.junit.Test;
+
+public class IntegerTest {
+
+  @Test
+  public void testIntegerToString(){
+    Integer.toString(100, 2).toUpperCase();
+  }
+
+}
